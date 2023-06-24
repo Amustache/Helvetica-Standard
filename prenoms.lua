e5b0d1675be44c0afbdbacc873d5b877f2d8132b
Stache <stache@stache.cat>
2023-06-24 21:53:10 +0200

    Reduce name list

--- prenoms.lua
+++ prenoms.lua
@@ -1,17 +1,8 @@
 local prenoms = {'', '', 'fabien', 'lothaire', 'monique', 'ninon', 'jolie', 'papillion', 'damien', 'noelle', 'gilberte',
-                 'olivie', 'maximilien', 'modestine', 'matthieu', 'eloi', 'denis', 'carole', 'thibault', 'paule',
-                 'adrienne', 'ghislaine', 'claudine', 'gratien', 'marielle', 'sylviane', 'virginie', 'raymonde', 'anne',
-                 'florianne', 'blanche', 'florentin', 'roxane', 'fabienne', 'rodrigue', 'arlette', 'michel', 'aristide',
-                 'augustin', 'rochelle', 'juliette', 'sylvaine', 'gilles', 'micheline', 'adrien', 'sabine', 'hercule',
-                 'rosine', 'madeleine', 'clarisse', 'sylvianne', 'olivier', 'didier', 'marceline', 'loup', 'clementine',
-                 'hugues', 'rolande', 'marcelline', 'constantin', 'philibert', 'marthe', 'angeline', 'hyacinthe',
-                 'reine', 'dianne', 'nicolas', 'mirabelle', 'bastien', 'marguerite', 'suzette', 'emmanuelle', 'gaston',
+                 'rosine', 'madeleine', 'clarisse', 'sylviane', 'olivier', 'didier', 'marceline', 'loup', 'clementine',
                  'gwenaelle', 'victoire', 'hortense', 'mathilde', 'anselme', 'sylvain', 'nadine', 'liane', 'angelique',
                  'christelle', 'laurentine', 'gustave', 'pons', 'zephyrine', 'oriane', 'agathe', 'justine', 'lucienne',
-                 'fiacre', 'philippe', 'aline', 'juste', 'lucinde', 'lazare', 'benjamine', 'pascal', 'seraphine',
                  'tatienne', 'prosper', 'mignon', 'noella', 'charlotte', 'renaud', 'narcisse', 'evette', 'yolande',
-                 'romaine', 'amaury', 'antoine', 'pascale', 'luc', 'marcelle', 'donatienne', 'lucille', 'joceline',
-                 'alexandrie', 'placide', 'rosette', 'toussaint', 'doriane', 'ghislain', 'lucile', 'bertrand', 'marise',
-                 '', '', ''}
+                 '', '', '', '', ''}
 
 return prenoms
