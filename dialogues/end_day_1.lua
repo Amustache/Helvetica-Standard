dd83cd79ede4f0045ba10c9633545ee569594cc6
Stache <stache@stache.cat>
2023-06-25 12:47:49 +0200

    Remove quit for end of day

--- dialogues/end_day_1.lua
+++ dialogues/end_day_1.lua
@@ -4,11 +4,8 @@ local script = Ero(function()
     name ""
     background "img/chapter.png"
     local continuer = option "Start a new day"
-    local quit_game = option "Quit"
     menu "End of the day.--"
     if selection(new_game) then
-    elseif selection(quit_game) then
-        love.event.quit()
     end
 end):defineAttributes({'name', 'avatar', 'background'})
 
